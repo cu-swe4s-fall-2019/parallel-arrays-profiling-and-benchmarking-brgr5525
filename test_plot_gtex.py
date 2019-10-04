3a78c2f8f7cc1fcca9dbaad014de2e1d253ed89b
Brianna Griffin <briannagriffin@Briannas-MacBook-Pro.local>
2019-10-03 19:31:33 -0600

    add unittesting for binary search

--- test_plot_gtex.py
+++ test_plot_gtex.py
@@ -1,5 +1,6 @@
 import unittest
 import plot_gtex
+import data_viz
 
 
 class TestPlotGtex(unittest.TestCase):
@@ -7,6 +8,13 @@ class TestPlotGtex(unittest.TestCase):
     def test_linear_search_empty_list(self):
         key = 'cheddar'
         L = []
+
+        r = plot_gtex.linear_search(key, L)
+        self.assertEqual(r, -1)
+
+    def test_linear_search_key_not_there(self):
+        key = 'mozzarella'
+        L = ['cheddar', 'brie', 'provolone', 'swiss', 'gouda']
 
         r = plot_gtex.linear_search(key, L)
         self.assertEqual(r, -1)
@@ -32,6 +40,40 @@ class TestPlotGtex(unittest.TestCase):
         r = plot_gtex.linear_search(key, L)
         self.assertEqual(r, 2)
 
+    def test_binary_search_empty_list(self):
+        key = 'cheddar'
+        L = []
+
+        r = plot_gtex.binary_search(key, L)
+        self.assertEqual(r, -1)
+
+    def test_binary_search_key_not_there(self):
+        key = 'mozzarella'
+        L = [['brie', 0], ['cheddar', 1], ['gouda', 2], ['provolone', 3]]
+
+        r = plot_gtex.binary_search(key, L)
+        self.assertEqual(r, -1)
+
+    def test_binary_search_key_first(self):
+        key = 'brie'
+        L = [['brie', 0], ['cheddar', 1], ['gouda', 2], ['provolone', 3]]
+
+        r = plot_gtex.binary_search(key, L)
+        self.assertEqual(r, 0)
+
+    def test_binary_search_key_last(self):
+        key = 'provolone'
+        L = [['brie', 0], ['cheddar', 1], ['gouda', 2], ['provolone', 3]]
+
+        r = plot_gtex.binary_search(key, L)
+        self.assertEqual(r, 3)
+
+    def test_binary_search_key_mid(self):
+        key = 'gouda'
+        L = [['brie', 0], ['cheddar', 1], ['gouda', 2], ['provolone', 3]]
+
+        r = plot_gtex.binary_search(key, L)
+        self.assertEqual(r, 2)
 
 
 if __name__ == '__main__':
